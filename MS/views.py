186626b322dd308122d2276d82931862137acc40
liuliang <liul2566@163.com>
2022-04-10 20:11:57 +0800

    本地运行

--- MS/views.py
+++ MS/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect, FileResponse
 from django.shortcuts import render
 from .forms import UploadFileForm
 from musicpy import *
+from musicpy.sampler import *
 # Imaginary function to handle an uploaded file.
 from .process import handle_uploaded_file
 
@@ -15,15 +16,31 @@ def upload_file(request):
         print(request.POST)
         print(request.FILES)
         f = request.FILES['file']
-        with open('in.mid', 'wb+') as destination:
-            for chunk in f.chunks():
-                destination.write(chunk)
         instr = int(request.POST['instr'])
-        a = read('in.mid')
-        a.clear_program_change()
-        a.change_instruments([instr])
-        write(a, name='out.mid')
-        file = open('out.mid', 'rb')
+        if instr >= 0:
+            with open('in.mid', 'wb+') as destination:
+                for chunk in f.chunks():
+                    destination.write(chunk)
+            if instr <= 127:
+                a = read('in.mid')
+                a.clear_program_change()
+                a.change_instruments([instr])
+                write(a, name='out.mid')
+                file = open('out.mid', 'rb')
+            elif instr == 200:
+                inPiece = read("in.mid")
+                test = sampler(1)
+                test.load(0, 'MS/sf2/8Rock11e.sf2')
+                test.export(inPiece,
+                        mode='wav',
+                        action='export',
+                        filename='out.wav',
+                        bpm=None,
+                        export_args={},
+                        show_msg=1)
+                file = open('out.wav', 'rb')
+        elif instr < 0:
+            pass
         response = FileResponse(file)
         return response
     else:
