06dbd77aa3b69cc34233a5cc7d72767e4edddec3
liuliang <liul2566@163.com>
2022-04-12 00:19:19 +0800

    增加可视化函数以备用

--- MS/views.py
+++ MS/views.py
@@ -8,6 +8,35 @@ from musicpy import *
 from musicpy.sampler import *
 # Imaginary function to handle an uploaded file.
 from .process import handle_uploaded_file
+import matplotlib as plt
+
+
+def Visualization(filepath):
+    a = read(filepath)
+    notes_set = {}  # 从C0到B8
+    notes_list = []
+    b = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', "A#", 'B']
+    c = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    for i in c:
+        for j in b:
+            z = j + str(i)
+            notes_set[z] = 0
+            notes_list.append(z)
+    a1_notes = [1]
+    for i in range(0, a.track_number):
+        a1_notes = a1_notes + a[i].content.notes
+        a1_notes = list(one for one in a1_notes if str(one) in notes_list)
+    y = []
+    m = len(a1_notes)
+    for i in range(0, m):
+        y.append(a1_notes[i].name + str(a1_notes[i].num))
+    for i in y:
+        notes_set[i] = notes_set[i] + 1
+    d1 = list(notes_set.keys())
+    d2 = list(notes_set.values())
+    plt.figure(figsize=(32, 10))
+    plt.bar(d1, d2)
+    plt.savefig('notes_frequency.jpg', dpi=400, bbox_inches='tight')
 
 
 def upload_file(request):
@@ -32,12 +61,12 @@ def upload_file(request):
                 test = sampler(1)
                 test.load(0, 'MS/sf2/8Rock11e.sf2')
                 test.export(inPiece,
-                        mode='wav',
-                        action='export',
-                        filename='out.wav',
-                        bpm=None,
-                        export_args={},
-                        show_msg=1)
+                            mode='wav',
+                            action='export',
+                            filename='out.wav',
+                            bpm=None,
+                            export_args={},
+                            show_msg=1)
                 file = open('out.wav', 'rb')
         elif instr < 0:
             pass
